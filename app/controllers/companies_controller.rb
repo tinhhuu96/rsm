b8cfe09dff103c9764be5780a90a39d78c51946e
tinhhuu96 <tinhhuu96@gmail.com>
2017-11-22 12:58:29 +0700

    finsh update company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,16 +1,34 @@
 class CompaniesController < ApplicationController
   layout "employer"
-  before_action :load_company, only: %i(show edit)
+  before_action :load_company, only: %i(show edit update)
+  before_action :load_current_user, only: %i(edit)
 
+  def show; end
+
+  def edit; end
+
+  def update
+    if @company.update_attributes company_params
+      flash[:success] = t "update_success"
+    else
+      flash[:danger] = t "can_not_update"
+    end
+    redirect_to company_path
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit :name, :address, :majors, :contact_person, :phone, :company_info
+  end
+
+  def load_current_user
+    @user = current_user
+  end
   def load_company
     @company = Company.find_by id: params[:id]
     return if @company
     flash[:danger] = t "can_not_find_company"
     redirect_to root_url
-  end
-
-  def show; end
-
-  def edit
   end
 end
---
+++ spec/factories/company.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :company do
+    name "Framgia_Inc"
+    address "Da nang"
+    phone "0123456789"
+    majors "IT_softeware"
+    contact_person "Nguyen Van B"
+    company_info "Framgia_Inc da nang"
+    url_company "https://recruit.framgia.vn"
+  end
+end
